5d23aace2b9bd73d03b91d7ce4bdb8be29a818c9
fouad97 <loukrichi21@gmail.com>
2020-06-23 10:34:12 +0100

    Updates

---
+++ precache-manifest.58bee89487f04ef75b7001540c62bfc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37168ee42377803d2f4c5afb8a3b4222",
+    "url": "/index.html"
+  },
+  {
+    "revision": "711145913e1a8a51bb64",
+    "url": "/static/css/main.10c071fb.chunk.css"
+  },
+  {
+    "revision": "5f751f476fe9701f3974",
+    "url": "/static/js/2.643e374b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.643e374b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "711145913e1a8a51bb64",
+    "url": "/static/js/main.5a7c6fab.chunk.js"
+  },
+  {
+    "revision": "f6b9869515e741c2f29d",
+    "url": "/static/js/runtime-main.cca3efaf.js"
+  }
+]);
